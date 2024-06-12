39df0c1694527d1681d91431ae5af55f4b0ac7b6
irsa abid <156125278+IrsaAbid123@users.noreply.github.com>
2024-06-12 16:19:14 +0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import Navigation from './src/navigation/Navigation'
+import InputText from './src/reuseableComponent/InputText'
+import MessageUs from './src/screens/MessageUs'
+import ThanksScreen from './src/screens/ThanksScreen'
+import ContactUs from './src/screens/MessageUs'
+
+const App = () => {
+  return (
+    <Navigation />
+  )
+}
+
+export default App
